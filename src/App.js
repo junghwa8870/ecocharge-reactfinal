659f163d22ce6031b336f86ba35f2597e5c36ac4
junghwa8870 <suejunghwa@gmail.com>
2024-06-18 13:00:55 +0900

    Feat. 파비콘 추가

--- src/App.js
+++ src/App.js
@@ -12,10 +12,13 @@ import Main from './components/pages/main/Main.js';
 import ScrollToTopButton from './components/layout/ScrollToTopButton.js';
 import ChargeSpotDetail from './components/pages/findcharge/ChargeSpotDetail.js';
 import NewInfoWrite from './components/pages/newinfo/newInfoWrite/NewInfoWrite.js';
+import Favicon from 'react-favicon';
 
 function App() {
   return (
     <>
+      <Favicon url='/favicon-32x32.png' />
+
       <div className='app' style={{ display: 'flex', flexDirection: 'column' }}>
         <Header />
         {/* <Main /> */}
