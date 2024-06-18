aa1476088888e7de31a5006c004fd43e91a1edde
junghwa8870 <suejunghwa@gmail.com>
2024-06-18 11:47:12 +0900

    Feat. 슬라이더 이미지 밑 차이름 클래스 추가

--- src/components/pages/main/simpleslider/SimpleSlider.js
+++ src/components/pages/main/simpleslider/SimpleSlider.js
@@ -16,21 +16,27 @@ function SimpleSlider() {
     <Slider {...settings}>
       <div className='imgbox'>
         <img className='mainCarImg' src='carA.png' alt='Slide 1' />
+        <div className='mainCarImgName'>2025 코란도 EV</div>
       </div>
       <div className='imgbox'>
         <img className='mainCarImg' src='carB.png' alt='Slide 2' />
+        <div className='mainCarImgName'>2025 EV3</div>
       </div>
       <div className='imgbox'>
         <img className='mainCarImg' src='carC.png' alt='Slide 3' />
+        <div className='mainCarImgName'>2024 캐딜락 리릭</div>
       </div>
       <div className='imgbox'>
         <img className='mainCarImg' src='carD.png' alt='Slide 4' />
+        <div className='mainCarImgName'>2024 지프 랭글러 4XE</div>
       </div>
       <div className='imgbox'>
         <img className='mainCarImg' src='carE.png' alt='Slide 5' />
+        <div className='mainCarImgName'>2024 아우디 SQ8 e-트론 스포트백</div>
       </div>
       <div className='imgbox'>
         <img className='mainCarImg' src='carF.png' alt='Slide 6' />
+        <div className='mainCarImgName'>아우디 Q8 e-트론 스포트백</div>
       </div>
     </Slider>
   );
