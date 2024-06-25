9ca1613f1dfa0446e50e0b1d28701c666b0b99fa
dudtjq <dudtjq0415@naver.com>
2024-06-25 16:01:22 +0900

    Feat: 게시판 async 관련 생성

--- src/components/pages/userBoard/UserBoard.js
+++ src/components/pages/userBoard/UserBoard.js
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './UserBoard.scss';
+import { useNavigate } from 'react-router-dom';
+import { BOARD, BOARD_REPLY } from '../../../config/host_config';
+import axiosInstance from '../../../config/axios-config';
+import { dark } from '@mui/material/styles/createPalette';
 
 const UserBoard = () => {
+  const redirection = useNavigate();
+
+  // 요소 취득 예정
+  // const data = JSON.stringify({
+  //   bWriter: $bWriter,
+  //   bContent: $bContent,
+  //   bAddress: $bAddress,
+  //   bTitle: $bTitle,
+  //   bProfileImage: $bProfileImage,
+  // });
+
+  // 게시판
+  const API_BASE_URL = BOARD;
+
+  const [board, setBoard] = useState([]);
+
+  // const [token, setToken] = useState('');
+
+  // 게시판 등록 추가 함수
+  const addBoard = async (
+    bWriter,
+    bContent,
+    bAddress,
+    bTitle,
+    bProfileImage,
+  ) => {
+    const newBoard = {
+      bWriter: bWriter,
+      bContent: bContent,
+      bAddress: bAddress,
+      bTitle: bTitle,
+      bProfileImage: bProfileImage,
+    };
+    handleRequest(
+      () => axiosInstance.post(API_BASE_URL, newBoard),
+      (data) => setBoard(dark.board),
+    );
+  };
+
+  // 할 일 삭제 처리 함수
+  const removeBoard = async (id) => {
+    handleRequest(
+      () => axiosInstance.delete(`${API_BASE_URL}/${id}`),
+      (data) => setBoard(data.todos),
+    );
+  };
+
   return <div>userBoard</div>;
 };
 
