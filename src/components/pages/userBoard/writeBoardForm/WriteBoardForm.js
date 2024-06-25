248b5852d099398794d7b23ad879c34e51a2c17b
dudtjq <dudtjq0415@naver.com>
2024-06-25 16:26:12 +0900

    Fix : host-config 파일 이름 수정

--- src/components/pages/userBoard/writeBoardForm/WriteBoardForm.js
+++ src/components/pages/userBoard/writeBoardForm/WriteBoardForm.js
@@ -1,13 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './WriteBoardForm.scss';
 import { Button, Form, FormGroup, FormText, Input, Label } from 'reactstrap';
 import { Grid } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
+import { BOARD } from '../../../../config/host-config';
 
 const WriteBoardForm = () => {
   const navigate = useNavigate();
+
+  // const $
+
+  // // 요소 취득 예정
+  // // const data = JSON.stringify({
+  // //   bWriter: $bWriter,
+  // //   bContent: $bContent,
+  // //   bAddress: $bAddress,
+  // //   bTitle: $bTitle,
+  // //   bProfileImage: $bProfileImage,
+  // // });
+
+  // // 게시판
+  // const API_BASE_URL = BOARD;
+
+  // const [board, setBoard] = useState([]);
+
+  // // const [token, setToken] = useState('');
+
+  // // 게시판 등록 추가 함수
+  // const addBoard = async (
+  //   bWriter,
+  //   bContent,
+  //   bAddress,
+  //   bTitle,
+  //   bProfileImage,
+  // ) => {
+  //   const newBoard = {
+  //     bWriter: bWriter,
+  //     bContent: bContent,
+  //     bAddress: bAddress,
+  //     bTitle: bTitle,
+  //     bProfileImage: bProfileImage,
+  //   };
+  //   handleRequest(
+  //     () => axiosInstance.post(API_BASE_URL, newBoard),
+  //     (data) => setBoard(dark.board),
+  //   );
+  // };
+
+  // // 할 일 삭제 처리 함수
+  // const removeBoard = async (id) => {
+  //   handleRequest(
+  //     () => axiosInstance.delete(`${API_BASE_URL}/${id}`),
+  //     (data) => setBoard(data.todos),
+  //   );
+  // };
 
   return (
     <Grid className='WboardFormContainer'>
