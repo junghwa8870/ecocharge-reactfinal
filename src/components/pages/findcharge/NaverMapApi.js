dfa89cd8668ceccfe716c56c2260eb72b779244c
wonjinseop <wonjinseop7@naver.com>
2024-06-27 12:59:59 +0900

    Docs: 네이버 맵 defaultCenter 현재 위치로 설정되게 바꿈

--- src/components/pages/findcharge/NaverMapApi.js
+++ src/components/pages/findcharge/NaverMapApi.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { NavermapsProvider } from 'react-naver-maps';
 import MapComponent from './MapComponent';
-const NaverMapApi = (props) => {
+const NaverMapApi = ({ lat, lng }) => {
   const id = process.env.REACT_APP_NAVER_MAP_CLIENT_ID;
   console.log(id);
   return (
     <NavermapsProvider ncpClientId={id} submodules={['geocoder']}>
-      <MapComponent />
+      <MapComponent lat={lat} lng={lng} />
     </NavermapsProvider>
   );
 };
