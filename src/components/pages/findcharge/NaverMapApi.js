0cf4675e151bbbed91e8074e81ab4583e3881a4c
wonjinseop <wonjinseop7@naver.com>
2024-07-03 09:38:47 +0900

    Docs: 네이버 맵 주소 검색 기능 추가

--- src/components/pages/findcharge/NaverMapApi.js
+++ src/components/pages/findcharge/NaverMapApi.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { NavermapsProvider } from 'react-naver-maps';
+import { NaverMapContext, NavermapsProvider } from 'react-naver-maps';
 import MapComponent from './MapComponent';
-const NaverMapApi = ({ lat, lng }) => {
+const NaverMapApi = ({ lat, lng, addr, setGeometricData }) => {
   const id = process.env.REACT_APP_NAVER_MAP_CLIENT_ID;
   console.log(id);
   return (
     <NavermapsProvider ncpClientId={id} submodules={['geocoder']}>
-      <MapComponent lat={lat} lng={lng} />
+      <MapComponent
+        lat={lat}
+        lng={lng}
+        addr={addr}
+        setGeometricData={setGeometricData}
+      />
     </NavermapsProvider>
   );
 };
